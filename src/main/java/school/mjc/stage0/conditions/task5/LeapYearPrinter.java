477ebe1ca4f6362977777cab2d80b1875e776720
Artyom7777 <iartiq@gmail.com>
2022-11-23 09:53:48 +0300

    Done

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -1,6 +1,13 @@
 package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
-    public void isLeapYear(int year) {
+
+        public void isLeapYear ( int year){
+        boolean leapyear = (year > 1584 && ((year % 400 == 0) || (year %4 == 0 && year % 100 != 0)));
+        if (leapyear)
+            System.out.println("leap");
+        else
+            System.out.println("not leap");
     }
 }
+
